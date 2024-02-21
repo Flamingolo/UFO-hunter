de3b2de6e8e8ba32f85c975cdaa0a10b256dccf1
Anti Erisalu <antikaz@gmail.com>
2024-02-21 17:43:29 +0200

    bouncing

--- js/flying.js
+++ js/flying.js
@@ -1,22 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     const duck = document.getElementById('duck');
     const maxX = window.innerWidth - duck.clientWidth;
+    const maxY = window.innerHeight - duck.clientHeight;
 
-    let direction = 1; // 1 for right, -1 for left
-    let currentX = 0;
+    let directionX = 1; // 1 for right, -1 for left
+    let directionY = 1; // 1 for down, -1 for up
+    let currentX = 100;
+    let currentY = 200;
 
-    console.log(window.innerWidth)
-
-    // Function to move the duck horizontally within the viewport
+    // Function to move the duck within the viewport
     function moveDuck() {
-        currentX += 5* direction; // You can adjust the speed by changing the value (5 in this case)
+        currentX += 3 * directionX; // You can adjust the speed by changing the value (5 in this case)
+        currentY += 5 * directionY; // You can adjust the speed by changing the value (5 in this case)
 
         // Change direction when reaching the edges
         if (currentX <= 0 || currentX >= maxX) {
-            direction *= -1;
+            directionX *= -1;
+        }
+
+        if (currentY <= 0 || currentY >= maxY) {
+            directionY *= -1;
         }
 
         duck.style.left = `${currentX}px`;
+        duck.style.top = `${currentY}px`;
 
         // Call moveDuck again on the next animation frame
         requestAnimationFrame(moveDuck);
